1a3750f3ba105db637d1481a8216f752422bec0b
Moise Germain <60044518+MoiseGermain@users.noreply.github.com>
2025-03-09 23:17:21 -0400

    Update user-data.sh

--- scripts/user-data.sh
+++ scripts/user-data.sh
@@ -1,1 +1,29 @@
-# User Data script for EC2 setup
+#!/bin/sh
+
+# Install a LAMP stack
+dnf install -y httpd wget php-fpm php-mysqli php-json php php-devel
+dnf install -y mariadb105-server
+dnf install -y httpd php-mbstring
+
+# Start the web server
+chkconfig httpd on
+systemctl start httpd
+
+# Install the web pages
+if [ ! -f /var/www/html/webapp.zip ]; then
+   cd /var/www/html
+   wget -O 'webapp.zip' 'https://example.com/webapp.zip'
+   unzip webapp.zip
+fi
+
+# Install AWS SDK for PHP
+if [ ! -f /var/www/html/aws.zip ]; then
+   cd /var/www/html
+   mkdir vendor
+   cd vendor
+   wget https://docs.aws.amazon.com/aws-sdk-php/v3/download/aws.zip
+   unzip aws.zip
+fi
+
+# Update existing packages
+dnf update -y
